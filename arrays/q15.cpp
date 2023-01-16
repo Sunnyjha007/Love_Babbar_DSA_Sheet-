d24201cb09ac1d80e3436f5a76c64e670ff575c1
sunny jha <78363637+Sunnyjha007@users.noreply.github.com>
2023-01-16 08:08:19 +0530

    Update q15.cpp

--- arrays/q15.cpp
+++ arrays/q15.cpp
@@ -1,53 +1,16 @@
-#include"header.h"
-//brute force o(n^2) -> 2 loops and check the condition
-//optimal o(nlogn)
- long long  mergetwo(long long *arr,long start,long end,long mid){
-	long long   count=0;
-	long n1 = mid-start+1;
-	long n2 = end - mid;
-	long left[n1];
-	long right[n2];
-	for(long i=0;i<n1;i++){
-		left[i] = arr[start+i];
-	}
-	for(long j=0;j<n2;j++){
-		right[j] = arr[mid+1+j];
-	}
-	int i=0,j=0,k=start;
-	while(i<n1&&j<n2){
-		if(left[i]<=right[j]){
-			arr[k++] = left[i++];
-		}else{
-			arr[k++] = right[j++];
-			count+= n1-i;
-		}
-	}
-	while(i<n1){
-		arr[k++] = left[i++];
-	}
-	while(j<n2){
-		arr[k++] = right[j++];
-	}
-       return count;
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int j = n-1;
+        while(j>0&&nums[j]<=nums[j-1])j--;
+        if(j==0){reverse(nums.begin(),nums.end());return;}
+        //[1,5,8,4,7,6,5,3,1]
+        int i=n-1;
+        while(i>0&&nums[j-1]>=nums[i])i--;
+        swap(nums[j-1],nums[i]);
+        reverse(nums.begin()+j,nums.end());
 
-}
-long long  merge_sort(long long  *arr,long  start,long end){
-	long long  count=0;
-         if(start<end){
-	int mid = (start+end)/2;
-	count+= merge_sort(arr,start,mid);
-	count+= merge_sort(arr,mid+1,end);
-	count+= mergetwo(arr,start,end,mid);
-	 }
-	return count;
-
-}
-
-long long int inversionCount(long long arr[], long long n)
-{
-    // Your Code Here
-    long long ans = merge_sort(arr,0,n-1);
-     
-}
-
+    }
+};
 
